d58f44118aef001ae26caea5ce0e05e2bd081d5b
alexfilatov2000 <tanyaarni@gmail.com>
2021-06-09 16:08:27 +0300

    test version

--- src/factories/comment.factory.ts
+++ src/factories/comment.factory.ts
@@ -7,16 +7,16 @@ import { findAllUsers } from '../models/userModels';
 //@ts-ignore
 define(Comment, async (faker: typeof Faker) => {
     // @ts-ignore
-    // const posts = await findAllPosts();
-    // const users = await findAllUsers();
-    // const randomContent = faker.lorem.paragraphs();
-    // const randomUserId = faker.random.arrayElement(users.map((user) => user.id));
-    // const randomPostId = faker.random.arrayElement(posts.map((post) => post.id));
-    //
-    // const comment = new Comment();
-    // comment.content = randomContent;
-    // comment.post_id = randomPostId;
-    // comment.user_id = randomUserId;
-    //
-    // return comment;
+    const posts = await findAllPosts();
+    const users = await findAllUsers();
+    const randomContent = faker.lorem.paragraphs();
+    const randomUserId = faker.random.arrayElement(users.map((user) => user.id));
+    const randomPostId = faker.random.arrayElement(posts.map((post) => post.id));
+
+    const comment = new Comment();
+    comment.content = randomContent;
+    comment.post_id = randomPostId;
+    comment.user_id = randomUserId;
+
+    return comment;
 });
--- src/factories/post.factory.ts
+++ src/factories/post.factory.ts
@@ -5,17 +5,17 @@ import { findAllUsers } from '../models/userModels';
 
 //@ts-ignore
 define(Post, async (faker: typeof Faker) => {
-    const users = await findAllUsers();
-    const randomTitle = faker.lorem.word();
-    const randomContent = faker.lorem.paragraphs();
-    const randomStatus: 'active' | 'inactive' = faker.random.arrayElement(['active', 'inactive']);
-    const randomUserId = faker.random.arrayElement(users.map((user) => user.id));
-
-    const post = new Post();
-    post.title = randomTitle;
-    post.content = randomContent;
-    post.status = randomStatus;
-    post.user_id = randomUserId;
-
-    return post;
+    // const users = await findAllUsers();
+    // const randomTitle = faker.lorem.word();
+    // const randomContent = faker.lorem.paragraphs();
+    // const randomStatus: 'active' | 'inactive' = faker.random.arrayElement(['active', 'inactive']);
+    // const randomUserId = faker.random.arrayElement(users.map((user) => user.id));
+    //
+    // const post = new Post();
+    // post.title = randomTitle;
+    // post.content = randomContent;
+    // post.status = randomStatus;
+    // post.user_id = randomUserId;
+    //
+    // return post;
 });
